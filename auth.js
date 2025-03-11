71baf1e52289b9ffb6ac5b9c5c347876c28369cc
Jihyun <wlguskim21@naver.com>
2025-03-11 21:18:39 +0900

    auth.js 생성, passwordVisibility 등록

---
+++ auth.js
@@ -0,0 +1,18 @@
+const visible = document.getElementsByClassName('show-password');
+const password = document.getElementById('password');
+const checkPassword = document.getElementById('check-password');
+
+function passwordVisibility (e) {
+    if (e.target.getAttribute("src") === "/img/visibility_off.png") {
+        e.target.setAttribute("src", "/img/visibility_on.png");
+        e.target.parentElement.previousElementSibling.setAttribute("type", "text");
+    } else {
+        e.target.setAttribute("src", "/img/visibility_off.png");
+        e.target.parentElement.previousElementSibling.setAttribute("type", "password");
+    }
+}
+
+console.log(visible);
+for (let icon of visible) {
+    icon.addEventListener('click', passwordVisibility);
+}
