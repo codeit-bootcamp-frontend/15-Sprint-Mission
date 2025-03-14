9507c29c3861dcabbf9565ed6c2c97437726d33a
Jihyun <wlguskim21@naver.com>
2025-03-14 22:46:38 +0900

    auth.js에 추가: input값 없을 시 error 메세지 출력

--- auth.js
+++ auth.js
@@ -1,3 +1,64 @@
+const email = document.getElementById('email-id');
+const nickname = document.getElementById('nickname-id')
+const pwd = document.getElementById('pwd-id');
+const errorMessage = document.getElementsByClassName('error-message');
+
+function emailError (e) {
+    if (e.target.value === '') {
+        for (elem in errorMessage) {
+            if (errorMessage[elem].classList.contains('email'))
+            errorMessage[elem].textContent = '이메일을 입력해주세요.';
+        }
+        e.target.classList.add('error');
+    } else {
+        for (elem in errorMessage) {
+            if (errorMessage[elem].classList.contains('email'))
+            errorMessage[elem].textContent = '';
+        }
+        e.target.classList.remove('error');
+    }
+}
+
+email.addEventListener('focusout', emailError);
+
+function pwdError (e) {
+    if (e.target.value === '') {
+        for (elem in errorMessage) {
+            if (errorMessage[elem].classList.contains('pwd'))
+            errorMessage[elem].textContent = '비밀번호를 입력해주세요.';
+        }
+        e.target.classList.add('error');
+    } else {
+        for (elem in errorMessage) {
+            if (errorMessage[elem].classList.contains('pwd'))
+            errorMessage[elem].textContent = '';
+        }
+        e.target.classList.remove('error');
+    }
+}
+
+pwd.addEventListener('focusout', pwdError);
+
+function nicknameError (e) {
+    if (e.target.value === '') {
+        for (elem in errorMessage) {
+            if (errorMessage[elem].classList.contains('nickname'))
+            errorMessage[elem].textContent = '닉네임을 입력해주세요.';
+        }
+        e.target.classList.add('error');
+    } else {
+        for (elem in errorMessage) {
+            if (errorMessage[elem].classList.contains('nickname'))
+            errorMessage[elem].textContent = '';
+        }
+        e.target.classList.remove('error');
+    }
+}
+
+if (nickname !== null) {
+    nickname.addEventListener('focusout', nicknameError);
+}
+
 const visible = document.getElementsByClassName('visible');
 
 function passwordVisibility (e) {
