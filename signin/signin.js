ef4d7555c95d06717d007576930d52ebdeb0c00d
YuWonKyu <duck01777@naver.com>
2025-03-17 18:17:00 +0900

    Add Signin.js 비밀번호 보기/가리기, 로그인 유효성
    - 로그인 유효성 확인 및 css 색상 변경
    - 비밀번호 눈모양 클릭식 비밀번호 보기/가리기 가능하게 변경

--- signin/signin.js
+++ signin/signin.js
@@ -1,12 +1,83 @@
+//이메일
 document.getElementById('email').addEventListener('blur', function(){
   const emailInput = document.getElementById('email');
-  const errorMassage = document.getElementById('error-message');
+  const errorMassage = document.getElementById('error-email-message');
+  const errorMassageFormat = document.getElementById('error-emailform-message')
 
-  if(!emailInput.value) {
+  if (emailInput.value.trim() === '') {
     emailInput.classList.add('error');
     errorMassage.style.display = 'block';
+    errorMassageFormat.style.display = 'none';
+  } else if (!isValidEmail(emailInput.value)) {
+    emailInput.classList.add('error');
+    errorMassageFormat.style.display = 'block';
+    errorMassage.style.display = 'none';
   } else {
     emailInput.classList.remove('error');
     errorMassage.style.display = 'none';
+    errorMassageFormat.style.display = 'none';
   }
-});
+})
+
+function isValidEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+}
+//비밀번호
+document.getElementById('password').addEventListener('blur', function(){
+  const passwordInput = document.getElementById('password');
+  const errorMassage = document.getElementById('error-password-message');
+  const errorMassageLength = document.getElementById('error-message-length');
+
+  if (passwordInput.value.trim() ==='') {
+    passwordInput.classList.add('error');
+    errorMassage.style.display = 'block';
+    errorMassageLength.style.display = 'none';
+  } else if (passwordInput.value.length < 8) {
+    passwordInput.classList.add('error');
+    errorMassageLength.style.display = 'block';
+    errorMassage.style.display = 'none';
+  } else {
+    passwordInput.classList.remove('error');
+    errorMassage.style.display = 'none';
+    errorMassageLength.style.display = 'none';
+  }
+  validateForm();
+})
+// 이메일 + 비밀번호 유효성
+function isValidPassword(password) {
+  return password.length >=8;
+}
+
+function validateForm() {
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+  const loginButton = document.getElementById('signin-button');
+
+  const emailValid = isValidEmail(emailInput.value.trim());
+  const passwordValid = isValidPassword(passwordInput.value.trim());
+
+  if (emailValid && passwordValid) {
+    loginButton.disabled = false;    
+  } else {
+    loginButton.disabled = true;
+  }
+}
+// 로그인 클릭시
+document.getElementById('signin-button').addEventListener('click',function(){
+  window.location.href = "/items";
+})
+
+//비밀번호 눈모양설정
+document.getElementById('toggle-password').addEventListener('click', function(){
+  const passwordInput = document.getElementById('password');
+  const eyeIcon = document.getElementById('eye-icon')
+
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    eyeIcon.src = '../img/passwordEye.svg';
+    } else {
+      passwordInput.type = 'password';
+    eyeIcon.src = '../img/passwordBlind.svg';
+    }
+})
--- signup/signup.js
+++ signup/signup.js
@@ -1,12 +1,81 @@
+//이메일
 document.getElementById('email').addEventListener('blur', function(){
   const emailInput = document.getElementById('email');
-  const errorMassage = document.getElementById('error-message');
+  const errorMassage = document.getElementById('error-email-message');
+  const errorMassageFormat = document.getElementById('error-emailform-message')
 
-  if(!emailInput.value) {
+  if (emailInput.value.trim() === '') {
     emailInput.classList.add('error');
     errorMassage.style.display = 'block';
+    errorMassageFormat.style.display = 'none';
+  } else if (!isValidEmail(emailInput.value)) {
+    emailInput.classList.add('error');
+    errorMassageFormat.style.display = 'block';
+    errorMassage.style.display = 'none';
   } else {
     emailInput.classList.remove('error');
     errorMassage.style.display = 'none';
+    errorMassageFormat.style.display = 'none';
   }
 });
+// 이메일 양식 검증
+function isValidEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+}
+//닉네임
+document.getElementById('username').addEventListener('blur', function(){
+  const usernameInput = document.getElementById('username');
+  const errorMassage = document.getElementById('error-username-message');
+
+  if(!usernameInput.value) {
+    usernameInput.classList.add('error');
+    errorMassage.style.display = 'block';
+  } else {
+    usernameInput.classList.remove('error');
+    errorMassage.style.display = 'none';
+  }
+});
+//비밀번호
+document.getElementById('password').addEventListener('blur', function(){
+  const passwordInput = document.getElementById('password');
+  const errorMassage = document.getElementById('error-password-message');
+  const errorMassageLength = document.getElementById('error-message-length');
+
+  if (passwordInput.value.trim() ==='') {
+    passwordInput.classList.add('error');
+    errorMassage.style.display = 'block';
+    errorMassageLength.style.display = 'none';
+  } else if (passwordInput.value.length < 8) {
+    passwordInput.classList.add('error');
+    errorMassageLength.style.display = 'block';
+    errorMassage.style.display = 'none';
+  } else {
+    passwordInput.classList.remove('error');
+    errorMassage.style.display = 'none';
+    errorMassageLength.style.display = 'none';
+  }
+  checkPasswordMatch();
+});
+//비밀번호 재확인
+document.getElementById('checkpassword').addEventListener('blur', function(){
+  checkPasswordMatch();
+});
+
+function checkPasswordMatch() {
+  const passwordInput = document.getElementById('password');
+  const checkpassword = document.getElementById('checkpassword');
+  const errorMassageMatch = document.getElementById('error-matchpassword-message');
+
+  if (passwordInput.value !== checkpassword.value) {
+    checkpassword.classList.add('error');
+    errorMassageMatch.style.display = 'block';
+  } else {
+    checkpassword.classList.remove('error');
+    errorMassageMatch.style.display = 'none';
+  }
+};
+//비밀번호 8자리 검증
+function isValidPassword(password) {
+  return password.length >=8;
+};
