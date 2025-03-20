bea741ca4a0e93f5d840b41bb6fcdf5ddd5c9f12
popo <hirouth@naver.com>
2025-03-21 02:31:26 +0900

    feat: toggleVisibleIcon 함수 로그인과 회원가입에서 재사용 가능하게 수정

--- js/toggleVisibleIcon.js
+++ js/toggleVisibleIcon.js
@@ -1,17 +1,23 @@
-// cdn에서 아이콘 불러오는 것은 비동기적
-// <i> 요소 존재하지 않을 수도 있으므로 HTML이 완전히 로드된 이후 사용하도록 함
+// cdn은 비동기 처리 및 <i> 요소가 실제 존재하지 않을 수 있으므로 페이지 로드가 다 되고 처리
+// 로그인 및 회원가입 input에서 공통적으로 사용
 
 document.addEventListener("DOMContentLoaded", () => {
-  const visibleIcon = document.querySelector(".password-toggle-icon i");
-  const passwordInput = document.querySelector("#password");
+  // 눈 모양 아이콘 선택 (NodeList로 반환)
+  const passwordToggles = document.querySelectorAll(".password-toggle-icon");
 
-  visibleIcon.parentElement.addEventListener("click", () => {
-    // 비밀번호 input 타입 토글
-    const isPasswordVisible = passwordInput.type === "text";
-    passwordInput.type = isPasswordVisible ? "password" : "text";
+  // 각 요소에 대한 이벤트 리스너 할당
+  passwordToggles.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+      const passwordInput = toggle.closest("div").querySelector("input"); // 가까운 조상 찾기
+      const visibleIcon = toggle.querySelector("i");
 
-    // <i> 태그 클래스 전환
-    visibleIcon.classList.toggle("fa-eye");
-    visibleIcon.classList.toggle("fa-eye-slash");
+      // 비밀번호 input 타입 토글
+      const isPasswordVisible = passwordInput.type === "text";
+      passwordInput.type = isPasswordVisible ? "password" : "text";
+
+      // <i> 태그 클래스 전환
+      visibleIcon.classList.toggle("fa-eye");
+      visibleIcon.classList.toggle("fa-eye-slash");
+    });
   });
 });
