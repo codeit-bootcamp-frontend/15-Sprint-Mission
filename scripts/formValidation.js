87de18e32e4c461df18eb356c6b5d4974aa1bf09
two678 <two6788@gmail.com>
2025-03-22 20:42:03 +0900

    feat: 비밀번호 입력값 유효성 검사 및 스타일 적용

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -1,7 +1,8 @@
 const email = document.getElementById('email');
 const password = document.getElementById('pwd');
 const username = document.getElementById('username');
-const message = document.getElementById('message');
+// const message = document.getElementById('message');
+let checkValid = true;
 
 document.documentElement.style.setProperty('--red', '#F74747');
 
@@ -15,6 +16,7 @@ function emailCheck(email_address){
 }
 
 email.addEventListener('focusout', function() {
+    const message = document.getElementsByClassName('message-email')[0];
     const inputValue = email.value;
 
     if(!emailCheck(inputValue)) {
@@ -24,10 +26,30 @@ email.addEventListener('focusout', function() {
             message.textContent = '잘못된 이메일 형식입니다';
         }
         email.style.border = '1px solid var(--red)';
+        checkValid = false;
     } else {
         message.textContent = '';
-        inputValue.style.border = 'none';
+        email.style.border = 'none';
+        checkValid = true;
     }
 });
 
 
+password.addEventListener('focusout', function() {
+    const message = document.getElementsByClassName('message-password')[0];
+    const inputValue = password.value;
+
+    if(!inputValue) {
+        message.textContent = '비밀번호를 입력해주세요';
+        password.style.border = '1px solid var(--red)';
+        checkValid = false;
+    } else if(inputValue.length < 8) {
+        message.textContent = '비밀번호를 8자 이상 입력해주세요';
+        password.style.border = '1px solid var(--red)';
+        checkValid = false;
+    } else {
+        message.textContent = '';
+        password.style.border = 'none';
+        checkValid = true;
+    }
+});
