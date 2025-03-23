80a490d5f34a7c64309409cb07e3dd8fc67339e7
leetaesik <vlwkvkd@naver.com>
2025-03-23 21:30:44 +0900

    이메일 형식에 맞지 않는 경우 추가

--- auth.js
+++ auth.js
@@ -4,32 +4,47 @@ const signupForm = document.querySelector(".signup-form");
 const setInputError = (event) => {
   console.dir(event.target);
   if (event.target.value === "") {
+    event.target.nextElementSibling?.remove();
     event.target.classList.add("input-error");
-    setErrorMessage(event.target);
+    setErrorMessage(event.target, "empty");
+  } else if (event.target.id === "useremail") {
+    event.target.nextElementSibling?.remove();
+    if (event.target.validationMessage) {
+      setErrorMessage(event.target, "email");
+    } else {
+      event.target.classList.remove("input-error");
+    }
   } else {
     event.target.classList.remove("input-error");
-    event.target.nextElementSibling.remove();
+    event.target.nextElementSibling?.remove();
   }
 };
 
-const setErrorMessage = (element) => {
-  if (!element.nextElementSibling) {
+const setErrorMessage = (element, type) => {
+  if (type === "empty") {
+    if (!element.nextElementSibling) {
+      const errorMessage = document.createElement("p");
+      errorMessage.classList.add("error-message", "text-lg", "semibold");
+      switch (element.id) {
+        case "useremail":
+          errorMessage.textContent = "이메일을 입력해주세요.";
+          break;
+        case "password":
+          errorMessage.textContent = "비밀번호를 입력해주세요.";
+          break;
+        case "nickname":
+          errorMessage.textContent = "닉네임을 입력해주세요.";
+          break;
+        case "checkPassword":
+          errorMessage.textContent = "비밀번호 확인을 입력해주세요.";
+          break;
+      }
+      element.after(errorMessage);
+    }
+  } else if (type === "email") {
     const errorMessage = document.createElement("p");
     errorMessage.classList.add("error-message", "text-lg", "semibold");
-    switch (element.id) {
-      case "useremail":
-        errorMessage.textContent = "이메일을 입력해주세요.";
-        break;
-      case "password":
-        errorMessage.textContent = "비밀번호를 입력해주세요.";
-        break;
-      case "nickname":
-        errorMessage.textContent = "닉네임을 입력해주세요.";
-        break;
-      case "checkPassword":
-        errorMessage.textContent = "비밀번호 확인을 입력해주세요.";
-        break;
-    }
+    errorMessage.textContent = "잘못된 이메일 형식입니다.";
     element.after(errorMessage);
   }
 };
