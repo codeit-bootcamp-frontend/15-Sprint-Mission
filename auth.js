722efb86f4fe83effa732117f8435572129b5b0b
leetaesik <vlwkvkd@naver.com>
2025-03-23 21:15:11 +0900

    input 값이 없을 경우 에러 메세지 추가

--- auth.js
+++ auth.js
@@ -2,10 +2,35 @@ const loginForm = document.querySelector(".login-form");
 const signupForm = document.querySelector(".signup-form");
 
 const setInputError = (event) => {
+  console.dir(event.target);
   if (event.target.value === "") {
     event.target.classList.add("input-error");
+    setErrorMessage(event.target);
   } else {
     event.target.classList.remove("input-error");
+    event.target.nextElementSibling.remove();
+  }
+};
+
+const setErrorMessage = (element) => {
+  if (!element.nextElementSibling) {
+    const errorMessage = document.createElement("p");
+    errorMessage.classList.add("error-message", "text-lg", "semibold");
+    switch (element.id) {
+      case "useremail":
+        errorMessage.textContent = "이메일을 입력해주세요.";
+        break;
+      case "password":
+        errorMessage.textContent = "비밀번호를 입력해주세요.";
+        break;
+      case "nickname":
+        errorMessage.textContent = "닉네임을 입력해주세요.";
+        break;
+      case "checkPassword":
+        errorMessage.textContent = "비밀번호 확인을 입력해주세요.";
+        break;
+    }
+    element.after(errorMessage);
   }
 };
 
