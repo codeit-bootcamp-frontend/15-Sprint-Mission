b176a1e1caa1a5c8a1ffa1188dc2882f94086822
two678 <two6788@gmail.com>
2025-04-08 17:23:17 +0900

    Refactor(entry): 앱 진입점 구조 개선을 위한 App 컴포넌트 도입
    - 기존 main.jsx의 루트 역할을 App.jsx로 분리
    - main.jsx는 App 컴포넌트를 렌더링하는 역할만 담당하도록 수정
    - 구조 정리 및 향후 유지보수를 위한 진입점 역할 명확화

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import AuthLayout from "./components/AuthLayout";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Items from "./pages/Items";
+import FAQ from "./pages/FAQ";
+import Privacy from "./pages/Privacy";
+import "./styles/reset.css";
+import "./styles/variable.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+        <Route path="/items" element={<Items />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/privacy" element={<Privacy />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,5 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import { BrowserRouter, Routes, Route } from "react-router";
-import HomePage from "./pages/Homepage";
-import AuthLayout from "./components/AuthLayout";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Items from "./pages/Items";
-import FAQ from "./pages/FAQ";
-import Privacy from "./pages/Privacy";
-import "./styles/reset.css";
-import "./styles/variable.css";
-
-function Main() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route element={<AuthLayout />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Route>
-        <Route path="/items" element={<Items />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/privacy" element={<Privacy />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default Main;
+import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
