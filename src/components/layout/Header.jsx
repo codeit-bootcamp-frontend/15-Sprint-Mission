e706634374da0a8cfbbc97981a6176d35761a70f
leetaesik <vlwkvkd@naver.com>
2025-04-11 15:57:46 +0900

    🎨 헤더 완성

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,11 +4,13 @@ import UserMenu from "../UserMenu";
 
 const Header = () => {
   return (
-    <div>
-      <Logo />
-      <Navbar />
-      <UserMenu />
-    </div>
+    <header className="sticky top-0 right-0 left-0 border-b border-solid border-b-[#DDE0E3]">
+      <div className="tablet:gap-9 mx-auto flex max-w-480 items-center gap-2 p-4">
+        <Logo />
+        <Navbar />
+        <UserMenu />
+      </div>
+    </header>
   );
 };
 export default Header;
