2ba2e44ecd9d011d6ec6692792eb04582ad824a0
leetaesik <vlwkvkd@naver.com>
2025-04-11 15:08:50 +0900

    🎨 로고 완성

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,4 +1,23 @@
+import { Link } from "react-router";
+import LogoSm from "../assets/logo-icon-sm.png";
+import LogoTypo from "../assets/logo-icon-typo.png";
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <div>
+      <Link to={"/"}>
+        <img
+          src={LogoTypo}
+          alt="판다마켓 모바일 로고"
+          className="tablet:hidden h-full w-full"
+        />
+        <img
+          src={LogoSm}
+          alt="판다마켓 로고"
+          className="tablet:block hidden h-full w-full"
+        />
+      </Link>
+    </div>
+  );
 };
 export default Logo;
