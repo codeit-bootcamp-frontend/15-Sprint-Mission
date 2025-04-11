b59497c6b25cfe44cc8577f0ae662c02e6e4c310
leetaesik <vlwkvkd@naver.com>
2025-04-11 14:55:44 +0900

    🚧 기본 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import ItemsPage from "./pages/ItemsPage";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="items" element={<ItemsPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
