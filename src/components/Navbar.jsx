8710b14e0ac1547d1e51d9dc7ed1131adafb959c
leetaesik <vlwkvkd@naver.com>
2025-04-11 15:57:33 +0900

    🎨 네비게이션 바 완성

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,15 @@
+import { NavLink } from "react-router";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <ul className="tablet:text-2lg tablet:gap-8 flex grow gap-2 text-lg font-bold">
+      <li>
+        <NavLink to="/">자유게시판</NavLink>
+      </li>
+      <li>
+        <NavLink to="/items">중고마켓</NavLink>
+      </li>
+    </ul>
+  );
 };
 export default Navbar;
