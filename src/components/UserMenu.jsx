94cc592f10c6e3c2e480ad2950e3dccb9d8f6e0a
leetaesik <vlwkvkd@naver.com>
2025-04-11 15:12:08 +0900

    🎨 유저메뉴 완성

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,4 +1,12 @@
+import DefaultProfile from "../assets/default-profile.png";
+
 const UserMenu = () => {
-  return <div>UserMenu</div>;
+  return (
+    <img
+      src={DefaultProfile}
+      alt="기본 프로필 이미지"
+      className="h-full w-full"
+    />
+  );
 };
 export default UserMenu;
