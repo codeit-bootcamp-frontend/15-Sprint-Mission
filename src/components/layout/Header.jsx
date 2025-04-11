ccf8a5fd49c38bb7cdb82226db2dbc6bc4786584
leetaesik <vlwkvkd@naver.com>
2025-04-11 12:07:28 +0900

    ✨ 헤더관련 파일 추가
    /layout에 Header.jsx와 그 안에 들어갈 컴포넌트들인 Logo, Navbar, UserMenu파일을 추가했습니다.

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,14 @@
+import Logo from "../Logo";
+import Navbar from "../Navbar";
+import UserMenu from "../UserMenu";
+
+const Header = () => {
+  return (
+    <div>
+      <Logo />
+      <Navbar />
+      <UserMenu />
+    </div>
+  );
+};
+export default Header;
