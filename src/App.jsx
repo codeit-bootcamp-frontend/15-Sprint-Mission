9fdea9b876489e10bcdc5d8973e9152ebcf0eb2d
Jihyun <wlguskim21@naver.com>
2025-05-01 20:02:56 +0900

    ✨ mainpage 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import MainPage from "./pages/MainPage";
+import ItemsPage from "./pages/ItemsPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/items" element={<ItemsPage />} />
       </Routes>
     </BrowserRouter>
   );
