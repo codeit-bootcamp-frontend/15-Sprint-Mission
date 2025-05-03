c4565f63cd01561d2c4e704097cb4eab0d4c8377
박광민 <rhkdals0242@naver.com>
2025-05-03 23:30:37 +0900

    feat: 제품 정보를 가져오는 getProducts 함수 추가

---
+++ src/api/getProducts.js
@@ -0,0 +1,29 @@
+const getProducts = async ({
+  page = 1,
+  pageSize = 10,
+  orderBy = "recent",
+  keyword = "",
+}) => {
+  try {
+    const query = new URLSearchParams({
+      page: page.toString(),
+      pageSize: pageSize.toString(),
+      orderBy,
+      keyword,
+    });
+
+    const res = await fetch(
+      `https://panda-market-api.vercel.app/products?${query.toString()}`
+    );
+
+    if (!res.ok) {
+      throw new Error("상품을 불러오는 데 실패했습니다.");
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
