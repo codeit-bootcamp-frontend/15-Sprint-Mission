9ef0b736a5ff4e99a1d8dbd4a62dbe73dbe4eb7c
sumin <tnals2565@naver.com>
2025-05-03 22:29:01 +0900

    feat: 파일 업로드 시 썸네일, 삭제버튼 구현 완료

--- src/pages/AddItem/components/AddItemImage.jsx
+++ src/pages/AddItem/components/AddItemImage.jsx
@@ -1,13 +1,26 @@
+import React, { useState } from 'react';
 import styles from '../styles/AddItemImage.module.css';
 
 export default function AddItemImage() {
+  const [fileImg, setFileImg] = useState(null);
+  const handleFileChange = (e) => {
+    setFileImg(e.target.files[0]);
+  }
   return (
     <div className={styles.addItemImage}>
-      <input type='file' name='file' id='file' />
+      <input type='file' name='file' id='file' onChange={handleFileChange} />
       <label htmlFor='file'>
         <img src='/public/images/common/ic_plus.svg' alt='이미지 등록' />
         이미지 등록
       </label>
+      {fileImg && (
+        <div className={styles.addItemImagePreview}>
+          <img src={fileImg ? URL.createObjectURL(fileImg) : ''} alt='이미지 미리보기' />
+          <button type='button' onClick={() => setFileImg(null)}>
+          <img src='/public/images/common/ic_tag_x.svg' alt='이미지 삭제' />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
