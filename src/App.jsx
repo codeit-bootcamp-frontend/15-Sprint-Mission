d2e874616300c9d8bbf5d4ceb10911aa0e115c35
박광민 <rhkdals0242@naver.com>
2025-05-05 02:41:12 +0900

    feat: AddItem 경로를 App 컴포넌트에 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Items from "./pages/Items.jsx";
+import AddItem from "./pages/AddItem.jsx";
 import "./assets/styles/base.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/" replace />} />
         <Route path="/items" element={<Items />} />
+        <Route path="/additem" element={<AddItem />} />
       </Routes>
     </>
   );
