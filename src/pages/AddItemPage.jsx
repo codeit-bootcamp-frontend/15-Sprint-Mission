43a128d04334e0501b7d81b1f841fbe5dfb19810
Jihyun <wlguskim21@naver.com>
2025-05-06 14:20:02 +0900

    ✨ AddItemPage에 Header 추가

--- src/pages/AddItemPage.jsx
+++ src/pages/AddItemPage.jsx
@@ -1,5 +1,11 @@
+import Header from "../components/Header";
+
 function AddItemPage() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default AddItemPage;
