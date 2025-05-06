60bbf7d72baee936c150abb03fedfe8f8e0ab1b3
evaain706 <evaain706@gmail.com>
2025-05-06 22:56:10 +0900

    레이아웃 수정

--- react-page/src/components/ItemCard.jsx
+++ react-page/src/components/ItemCard.jsx
@@ -1,10 +1,10 @@
 const ItemCard = ({ item }) => {
   return (
-    <div className='rounded-md p-2 transition h-[31.7rem] w-[22.1rem] flex flex-col '>
+    <div className='rounded-md p-2 transition h-[26.4.7rem] w-[16.8rem] md:w-[22.1rem] md:h-[31.7rem] flex flex-col '>
       <img
         src={item.images[0]}
         alt={item.name}
-        className='w-full h-[22.1rem] object-cover rounded'
+        className='w-full h-[16.8rem] md:h-[31.7rem] object-cover rounded'
       />
       <div className='mt-[1.6rem] flex flex-col gap-3'>
         <div className='text-[1.4rem] text-gray-500 '>{item.name}</div>
