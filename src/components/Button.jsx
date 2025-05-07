3e8da571eb02bf2e547e70fe1b2516e75b9dbbe7
Jihyun <wlguskim21@naver.com>
2025-05-07 16:04:29 +0900

    💄 MainPage 버튼 디자인 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,12 @@
 function Button({ children, type, onClick, disabled }) {
   const buttonStyles = {
+    login: "w-128 h-48 rounded-lg bg-blue100 text-lg text-white font-semibold",
+    shopping:
+      "w-357 h-56 rounded-[40px] bg-blue100 text-2lg tablet:text-xl text-white font-semibold",
     additem:
       "py-8 px-24 rounded-lg bg-blue100 text-lg text-white font-semibold",
     upload:
-      "w-74 h-42 rounded-lg bg-blue100 text-lg text-white font-semibold disabled:bg-gray400",
+      "w-74 h-42 rounded-lg bg-blue100 text-lg text-white font-semibold disabled:bg-gray400 disabled:cursor-not-allowed",
   };
 
   return (
