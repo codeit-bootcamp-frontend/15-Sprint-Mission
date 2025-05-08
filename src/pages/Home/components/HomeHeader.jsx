fea55bffe68c5baf48df37523dd9120dad09a749
two678 <two6788@gmail.com>
2025-05-08 13:03:26 +0900

    ♻️ Refactor: Home 페이지 로고 반응형 수정

--- src/pages/Home/components/HomeHeader.jsx
+++ src/pages/Home/components/HomeHeader.jsx
@@ -3,13 +3,42 @@ import { Logo } from "@/components/Logo";
 import logo from "@/assets/logo/logo.png";
 import logoMobile from "@/assets/logo/logo_mobile.png";
 import styles from "../styles/HomeHeader.module.scss";
+import { useState, useEffect } from "react";
 
 function HomeHeader() {
+  const [logoImg, setLogoImg] = useState(logo);
+  const [logoSize, setLogoSize] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setLogoImg(logoMobile);
+        setLogoSize(false);
+      } else {
+        setLogoImg(logo);
+        setLogoSize(true);
+      }
+    };
+
+    // 초기 로드시 한번 실행
+    handleResize();
+
+    // 리사이즈 이벤트 리스너 등록
+    window.addEventListener("resize", handleResize);
+
+    // 클린업 함수
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <header className={styles.nav}>
       <div className={styles.logoWrapper}>
-        <Logo srcLogo={logo} imgClass={styles.desktopLogo} />
-        <Logo srcLogo={logoMobile} imgClass={styles.mobileLogo} />
+        <Logo
+          srcLogo={logoImg}
+          variant={logoSize ? "homeDesktop" : "homeMobile"}
+        />
       </div>
       <Link to="/login" className={styles.loginBtn}>
         로그인
