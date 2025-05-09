91684f3d34afc7b08d02775de5f43d3c08fa1315
two678 <two6788@gmail.com>
2025-05-09 21:04:53 +0900

    💄 Style: 홈페이지 푸터 이모션 적용

--- src/pages/Home/components/HomeFooter.jsx
+++ src/pages/Home/components/HomeFooter.jsx
@@ -1,42 +1,51 @@
+/** @jsxImportSource @emotion/react */
 import { Link } from "react-router-dom";
-import styles from "../styles/HomeFooter.module.scss";
 import faceBook from "@/assets/icons/ic_facebook.png";
 import twitter from "@/assets/icons/ic_twitter.png";
 import youtube from "@/assets/icons/ic_youtube.png";
 import instagram from "@/assets/icons/ic_instagram.png";
+import {
+  footer,
+  footerContainer,
+  heading,
+  privacyFAQ,
+  policyLinks,
+  icons,
+  iconImg,
+} from "../styles/HomeFooter.styles";
 
 function HomeFooter() {
   return (
-    <footer>
-      <div className={styles.footerContainer}>
-        <h5>@codeit-2024</h5>
-        <div className={styles.privacyFAQ}>
-          <Link to="/privacy.html" className={styles.policyLinks}>
+    <footer css={footer}>
+      <div css={footerContainer}>
+        <h5 css={heading}>@codeit-2024</h5>
+        <div css={privacyFAQ}>
+          <Link to="/privacy.html" css={policyLinks}>
             Privacy Policy
           </Link>
-          <Link to="/faq.html" className={styles.policyLinks}>
+          <Link to="/faq.html" css={policyLinks}>
             FAQ
           </Link>
         </div>
-        <ul className={styles.icons}>
+        <ul css={icons}>
           <li>
             <a href="https://www.facebook.com/">
-              <img src={faceBook} alt="페이스북 아이콘" />
+              <img src={faceBook} alt="페이스북 아이콘" css={iconImg} />
             </a>
           </li>
           <li>
             <a href="https://x.com/">
-              <img src={twitter} alt="트위터 아이콘" />
+              <img src={twitter} alt="트위터 아이콘" css={iconImg} />
             </a>
           </li>
           <li>
             <a href="https://www.youtube.com/">
-              <img src={youtube} alt="유튜브 아이콘" />
+              <img src={youtube} alt="유튜브 아이콘" css={iconImg} />
             </a>
           </li>
           <li>
             <a href="https://www.instagram.com/">
-              <img src={instagram} alt="인스타 아이콘" />
+              <img src={instagram} alt="인스타 아이콘" css={iconImg} />
             </a>
           </li>
         </ul>
