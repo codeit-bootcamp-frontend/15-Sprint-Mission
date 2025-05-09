decf210134cd484e6b9e97c4db04ddf41f4dd4a6
sumin <tnals2565@naver.com>
2025-05-09 20:53:32 +0900

    feat: 상품 상세페이지 문의하기 textarea 유효성 체크 기능 추가

--- src/pages/ItemDetail/components/ItemDetailInquiry.jsx
+++ src/pages/ItemDetail/components/ItemDetailInquiry.jsx
@@ -1,15 +1,24 @@
 import styles from '../styles/ItemDetailInquiry.module.css';
 import CommonButton from '@/components/common/CommonButton';
+import { useState } from 'react';
 
 export default function ItemDetailInquiry() {
+  const [inquiry, setInquiry] = useState('');
+
   return (
     <form className={styles.itemDetailInquiry}>
       <label htmlFor='inquiry'>문의하기</label>
       <textarea
         id='inquiry'
         placeholder='개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다.'
+        value={inquiry}
+        onChange={(e) => setInquiry(e.target.value)}
       />
-      <CommonButton  buttonType={{buttonType: 'submit', buttonStyle: 'primary', buttonText: '등록'}} disabled={true} />
+      <InquiryButton disabled={inquiry.length === 0} />
     </form>
   );
 }
+
+const InquiryButton = ({ disabled }) => {
+  return <CommonButton buttonType={{ buttonType: 'submit', buttonStyle: 'primary', buttonText: '등록' }} disabled={disabled} />;
+};
