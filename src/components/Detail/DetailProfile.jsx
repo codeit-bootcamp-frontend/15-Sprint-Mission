fd9cc77dfe065e24227c5002a410b5f45e2c4a75
sumin <tnals2565@naver.com>
2025-05-10 00:43:38 +0900

    feat: 상품 상세 페이지 반응형 작업 완료

--- src/components/Detail/DetailProfile.jsx
+++ src/components/Detail/DetailProfile.jsx
@@ -2,7 +2,7 @@ import styles from './styles/DetailProfile.module.css';
 
 export default function DetailProfile({ profileImage, profileNickname, profileUpdate, isComment=false}) {
   return (
-    <div className={styles.profile}>
+    <div className={`${styles.profile} ${isComment ? styles.isComment : ''}`}>
       <div className={styles.profileImage}>
         <img
           src={profileImage || '/images/common/ic_log.svg'}
@@ -13,7 +13,7 @@ export default function DetailProfile({ profileImage, profileNickname, profileUp
           }}
         />
       </div>
-      <div className={isComment ? styles.isComment : ''}>
+      <div>
         <p className={styles.profileNickName}>{profileNickname}</p>
         <p className={styles.profileUpdate}>{profileUpdate}</p>
       </div>
