edf5630b8465131bf65bbb33c477bafaaf8095dd
YuWonKyu <duck01777@naver.com>
2025-05-11 02:08:54 +0900

    스타일 변수 이름 변경 및 Nav 컴포넌트에서 Link를 NavLink로 변경하여 활성화 상태 스타일 추가

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,30 +1,36 @@
 import logoText from "../img/logoText.svg";
 import loginState from "../img/loginState.svg";
 import Favicon from "../img/favicon.svg";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className="flex items-center justify-between px-[16px] py-[15px] bg-[#ffffff] border-b border-[secondary-350]">
-      <div className="flex items-center gap-[8px]">
-        <Link to="/">
+    <div className="flex items-center justify-between px-16 py-15 bg-[#ffffff] border-b border-secondary-350">
+      <div className="flex items-center gap-8">
+        <NavLink to="/" className="flex items-center gap-8">
           <img
             src={Favicon}
             alt="Logo"
             className="size-40 hidden tablet:block"
           />
-          <img src={logoText} alt="Logo" className="w-[81px] h-[40px]" />
-        </Link>
-        <ul className="flex space-x-8 text-lg font- bold">
+          <img src={logoText} alt="Logo" className="w-81 h-40" />
+        </NavLink>
+        <ul className="flex space-x-8 text-lg font-bold text-secondary-600">
           <li>
-            <Link to="/freeboard" className="text-gray-700 hover:text-blue-500">
+            <NavLink
+              to="/freeboard"
+              className={({ isActive }) => (isActive ? "text-primary-100" : "")}
+            >
               자유게시판
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link to="/items" className="text-gray-700 hover:text-blue-500">
+            <NavLink
+              to="/items"
+              className={({ isActive }) => (isActive ? "text-primary-100" : "")}
+            >
               중고마켓
-            </Link>
+            </NavLink>
           </li>
         </ul>
       </div>
