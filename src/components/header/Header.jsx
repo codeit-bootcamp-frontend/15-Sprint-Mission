53e1bc47db479749eec078c6895b7e7d13af1c30
leetaesik <vlwkvkd@naver.com>
2025-05-11 04:13:38 +0900

    🎨 bg색 추가

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import UserMenu from "./UserMenu";
 
 const Header = () => {
   return (
-    <header className="sticky inset-x-0 top-0 border-b border-solid border-b-[#DDE0E3]">
+    <header className="sticky inset-x-0 top-0 border-b border-solid border-b-[#DDE0E3] bg-white">
       <div className="pc:gap-47 tablet:gap-35 mx-auto flex max-w-1920 items-center gap-8 p-16">
         <Logo />
         <Navbar />
