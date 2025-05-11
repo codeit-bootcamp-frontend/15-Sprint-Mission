b04700e2608afdf47452740b1d305990ce41853b
YuWonKyu <duck01777@naver.com>
2025-05-11 13:48:37 +0900

    BestItem 컴포넌트 추가 및 Nav 컴포넌트 스타일 수정: 고정 헤더로 변경하고 이미지 alt 텍스트를 한국어로 업데이트

---
+++ src/components/BestItem.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const BestItem = () => {
+  const [items, setItems] = useState([]);
+
+  const [pageSize, setPageSize] = useState(1);
+
+  // 화면 크기에 따라 pageSize 설정
+  // 모바일: 1, 태블릿: 2, PC: 4
+  const updatePageSize = () => {
+    const width = window.innerWidth;
+    if (width <= 744) {
+      setPageSize(1); // 모바일
+    } else if (width >= 744 && width < 1200) {
+      setPageSize(2); // 태블릿
+    } else {
+      setPageSize(4); // PC
+    }
+  };
+
+  // API 호출
+  useEffect(() => {
+    updatePageSize();
+    window.addEventListener("resize", updatePageSize); // 리사이즈 이벤트
+
+    const fetchItems = async () => {
+      try {
+        const response = await axios.get(
+          "https://panda-market-api.vercel.app/products",
+          {
+            params: {
+              page: 1,
+              pageSize: pageSize,
+              orderBy: "favorite",
+            },
+            headers: {
+              Accept: "application/json",
+            },
+          }
+        );
+        setItems(response.data.list || []);
+      } catch (error) {
+        console.error("상품을 불러오는 데 실패했습니다.", error);
+      }
+    };
+
+    fetchItems();
+
+    // 클린업: 리사이즈 이벤트 제거
+    return () => window.removeEventListener("resize", updatePageSize);
+  }, [pageSize]); // pageSize 변경 시 재호출
+
+  return (
+    <div className=" pt-87 w-344 tablet:w-744 pc:w-1200 mx-auto">
+      <h1 className="text-xl font-bold text-secondary-900 ">베스트 상품</h1>
+      <div
+        className="
+          grid
+          grid-cols-1
+          tablet:grid-cols-2
+          pc:grid-cols-4
+          gap-10
+          mt-16
+          justify-items-center
+          
+          "
+      >
+        {items.map((item) => (
+          <div
+            key={item.id}
+            className="flex flex-col gap-6 min-w-344 max-w-344 pc:min-w-282 pc:max-w-282"
+          >
+            <img
+              src={item.images[0]}
+              alt={item.name}
+              className="size-344 pc:size-282 object-cover rounded-xl mb-10"
+            />
+            <h2 className="text-md font-medium text-secondary-800">
+              {item.name}
+            </h2>
+            <p className="text-lg font-bold text-secondary-800">
+              {item.price.toLocaleString()}원
+            </p>
+            <p className="text-xs font-medium text-secondary-600">
+              ♡ {item.favoriteCount}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BestItem;
