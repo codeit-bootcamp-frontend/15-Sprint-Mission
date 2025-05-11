9570b0d4d96129fb79b14bb891f97d81c2501644
leetaesik <vlwkvkd@naver.com>
2025-05-11 20:26:13 +0900

    ✨ input 컴포넌트 생성

---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+const Input = ({ placeholder, className }) => {
+  return (
+    <input
+      placeholder={placeholder}
+      className={`font-regular focus:outline-primary-100 text-secondary-800 placeholder:text-secondary-400 bg-secondary-100 rounded-[12px] px-24 py-15 text-lg ${className}`}
+    />
+  );
+};
+export default Input;
