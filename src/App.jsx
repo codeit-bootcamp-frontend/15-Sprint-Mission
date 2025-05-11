932580eaf16448a43cc829067e5af8d4ab1b414f
박광민 <rhkdals0242@naver.com>
2025-05-11 23:09:43 +0900

    feat: 상품 상세 페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Items from "./pages/Items/Items.jsx";
 import AddItem from "./pages/AddItem/AddItem.jsx";
+import ProductDetail from "./pages/ProductDetail/ProductDetail.jsx";
 import "./assets/styles/base.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Navigate to="/" replace />} />
         <Route path="/items" element={<Items />} />
         <Route path="/additem" element={<AddItem />} />
+        <Route path="/items/:productId" element={<ProductDetail />} />
       </Routes>
     </>
   );
