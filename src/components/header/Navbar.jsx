89aa6de32aa59274c4ccc68b014ab1f759d85eb1
leetaesik <vlwkvkd@naver.com>
2025-05-11 22:23:59 +0900

    ✨ 페이지 주소가 additem일 때 네브바 색상 변경

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,6 +1,8 @@
-import { NavLink } from "react-router";
+import { NavLink, useLocation } from "react-router";
 
 const Navbar = () => {
+  const location = useLocation();
+  console.log(location);
   return (
     <ul className="tablet:text-2lg tablet:gap-30 text-secondary-600 flex grow gap-8 text-lg font-bold">
       <li>
@@ -9,7 +11,11 @@ const Navbar = () => {
       <li>
         <NavLink
           to="/items"
-          className={({ isActive }) => (isActive ? "text-primary-100" : "")}
+          className={({ isActive }) =>
+            isActive || location.pathname === "/additem"
+              ? "text-primary-100"
+              : ""
+          }
         >
           중고마켓
         </NavLink>
