78685f0b0ccb1928f6a82cf7fa1aab57d5bb4f71
leetaesik <vlwkvkd@naver.com>
2025-05-11 21:44:03 +0900

    🎨 태그 x아이콘 커서 포인터 추가

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -4,7 +4,7 @@ const Tag = ({ children, onDelete }) => {
   return (
     <div className="bg-secondary-100 text-secondary-800 font-regular flex items-center justify-between gap-8 rounded-[26px] py-5 pr-12 pl-16 text-lg">
       {children}
-      <button onClick={() => onDelete(children)}>
+      <button onClick={() => onDelete(children)} className="cursor-pointer">
         <img src={XIcon} alt="x아이콘" className="size-20" />
       </button>
     </div>
