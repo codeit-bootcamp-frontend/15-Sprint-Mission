d9217ad01675285e6209c79693fb20c54ec06334
leetaesik <vlwkvkd@naver.com>
2025-05-11 11:17:34 +0900

    ✨ 버튼 컴포넌트 onClick기능 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
-const Button = ({ children, className }) => {
+const Button = ({ children, className, onClick }) => {
   return (
     <button
       className={`bg-primary-100 hover:bg-primary-200 active:bg-primary-300 text-secondary-100 cursor-pointer rounded-[8px] px-23 py-8 text-lg font-semibold ${className}`}
+      onClick={onClick}
     >
       {children}
     </button>
