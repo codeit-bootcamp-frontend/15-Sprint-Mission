0980966e6600104f7314041eaeca29006f13eb2e
leetaesik <vlwkvkd@naver.com>
2025-05-11 20:31:01 +0900

    🎨 search에 input 컴포넌트 사용

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,10 @@
+import Input from "./Input";
+
 const Search = ({ className }) => {
   return (
-    <input
+    <Input
       placeholder="검색할 상품을 입력해주세요"
-      className={`font-regular bg-secondary-100 rounded-[12px] bg-[url(./assets/icons/icon-search.png)] bg-size-[24px_24px] bg-position-[16px] bg-no-repeat py-9 pr-20 pl-44 text-lg ${className}`}
+      className={`h-42 bg-[url(./assets/icons/icon-search.png)] bg-size-[24px_24px] bg-position-[16px] bg-no-repeat pl-44 ${className}`}
     />
   );
 };
