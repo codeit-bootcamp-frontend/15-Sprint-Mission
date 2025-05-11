7df80f4e4343d312db89ee35a6bab115b1ae1c56
박광민 <rhkdals0242@naver.com>
2025-05-11 23:10:09 +0900

    feat: ProductCard에 Link 추가하여 상세 페이지로 이동 가능하도록 수정

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,26 +1,29 @@
+import { Link } from "react-router-dom";
 import styles from "./ProductCard.module.css";
 import heartIcon from "../../assets/icon/heart-icon.svg";
 
-const ProductCard = ({ imageUrl, title, price, likes, variant }) => {
+const ProductCard = ({ id, imageUrl, title, price, likes, variant }) => {
   return (
-    <div className={styles.card}>
-      <img
-        src={imageUrl || "/no-img.svg"}
-        alt={title}
-        className={variant === "best" ? styles.bestImg : styles.allImg}
-        onError={(e) => {
-          e.currentTarget.src = "/no-img.svg";
-        }}
-      />
-      <div className={styles.info}>
-        <p className={styles.title}>{title}</p>
-        <p className={styles.price}>{price.toLocaleString()}원</p>
-        <p className={styles.likes}>
-          <img src={heartIcon} alt="좋아요" className={styles.icon} />
-          <span className={styles.count}>{likes}</span>
-        </p>
+    <Link to={`/items/${id}`} className={styles.cardLink}>
+      <div className={styles.card}>
+        <img
+          src={imageUrl || "/no-img.svg"}
+          alt={title}
+          className={variant === "best" ? styles.bestImg : styles.allImg}
+          onError={(e) => {
+            e.currentTarget.src = "/no-img.svg";
+          }}
+        />
+        <div className={styles.info}>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.price}>{price.toLocaleString()}원</p>
+          <p className={styles.likes}>
+            <img src={heartIcon} alt="좋아요" className={styles.icon} />
+            <span className={styles.count}>{likes}</span>
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
