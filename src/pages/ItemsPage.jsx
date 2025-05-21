648ebfab7c000879952bffe2741434d76cbcb1a5
YuWonKyu <duck01777@naver.com>
2025-05-21 15:04:16 +0900

    페이지네이션을 allitems로 이동

--- src/pages/ItemsPage.jsx
+++ src/pages/ItemsPage.jsx
@@ -1,13 +1,11 @@
 import BestItem from "../components/BestItem";
 import AllItems from "../components/AllItems";
-import Pagination from "../components/Pagination";
 
 const ItemsPage = () => {
   return (
     <>
       <BestItem />
       <AllItems />
-      <Pagination />
     </>
   );
 };
