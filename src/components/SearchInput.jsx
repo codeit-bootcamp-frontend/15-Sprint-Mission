972e4b1e483891752ad799ac2a9e2d43029cb95a
YuWonKyu <duck01777@naver.com>
2025-05-21 15:02:20 +0900

    ui-인풋h 수정

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -13,7 +13,7 @@ function SearchInput({ value, onChange }) {
         placeholder="검색할 상품을 입력해주세요"
         value={value}
         onChange={onChange}
-        className="focus:outline-none pl-28 "
+        className="focus:outline-none pl-28 w-256 h-24 "
       />
     </div>
   );
